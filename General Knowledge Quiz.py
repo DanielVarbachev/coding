dcc9c4900d80f89587bce33312bad30cc8029068
DanielVarbachev <danielvarbachev@gmail.com>
2024-07-16 19:12:50 +0100

    Add files via upload

---
+++ General Knowledge Quiz.py
@@ -0,0 +1,577 @@
+
+#The General Knowledge Quiz
+
+#Variables
+score = 0
+
+#Intro
+print("This is The General Knowledge Quiz!" )
+print("")
+print("By the end of the quiz you will find out whether you are extremely smart or very dumb." )
+print("")
+print("Answer a question by putting either a, b, c or d. ")
+print("")
+ready = input("Press enter to progress ")
+
+
+#Question 1
+print("")
+print("")
+print("")
+print("Question 1")
+print("What is the capital of France? ")
+print("")
+print("A: Paris")
+print("")
+print("B: Rome")
+print("")
+print("C: Madrid")
+print("")
+print("D: Berlin")
+print("")
+
+Q1_Answer = input(">>> ")
+
+if Q1_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, Paris")
+ready = input("Press enter to move onto the next question ")
+
+#Question 2
+print("")
+print("")
+print("")
+print("Question 2")
+print("Which planet is known as the “Red Planet”? ")
+print("")
+print("A: Venus")
+print("")
+print("B: Mars")
+print("")
+print("C: Jupiter")
+print("")
+print("D: Saturn")
+print("")
+
+Q2_Answer = input(">>> ")
+
+if Q2_Answer.lower() == "b":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was B, Mars")
+ready = input("Press enter to move onto the next question ")
+
+#Question 3
+print("")
+print("")
+print("")
+print("Question 3")
+print("Who wrote the play “Romeo and Juliet”?")
+print("")
+print("A: William Shakespeare")
+print("")
+print("B: Jane Austen")
+print("")
+print("C: Charles Dickens")
+print("")
+print("D: Mark Twain")
+print("")
+
+Q3_Answer = input(">>> ")
+
+if Q3_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, William Shakespeare")
+ready = input("Press enter to move onto the next question ")
+
+#Question 4
+print("")
+print("")
+print("")
+print("Question 4")
+print("What is the chemical symbol for water?")
+print("")
+print("A: CH4")
+print("")
+print("B: CO2")
+print("")
+print("C: O2")
+print("")
+print("D: H2O")
+print("")
+
+Q4_Answer = input(">>> ")
+
+if Q4_Answer.lower() == "d":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was D, H2O")
+ready = input("Press enter to move onto the next question ")
+
+#Question 5
+print("")
+print("")
+print("")
+print("Question 5")
+print("Which famous scientist developed the theory of relativity?")
+print("")
+print("A: Isaac Newton")
+print("")
+print("B: Albert Einstein")
+print("")
+print("C: Galileo Galilei")
+print("")
+print("D: Marie Curie")
+print("")
+
+Q5_Answer = input(">>> ")
+
+if Q5_Answer.lower() == "b":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was B, Albert Einstein")
+ready = input("Press enter to move onto the next question ")
+
+#Question 6
+print("")
+print("")
+print("")
+print("Question 6")
+print("What is the tallest mountain in the world?")
+print("")
+print("A: Mount Kilimanjaro")
+print("")
+print("B: Mount Everest")
+print("")
+print("C: Mount Fuji")
+print("")
+print("D: Mount McKinley")
+print("")
+
+Q6_Answer = input(">>> ")
+
+if Q6_Answer.lower() == "b":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was B, Mount Everest")
+ready = input("Press enter to move onto the next question ")
+
+#Question 7
+print("")
+print("")
+print("")
+print("Question 7")
+print("Which country is known as the Land of the Rising Sun?")
+print("")
+print("A: China")
+print("")
+print("B: Japan")
+print("")
+print("C: South Korea")
+print("")
+print("D: Thailand")
+print("")
+
+Q7_Answer = input(">>> ")
+
+if Q7_Answer.lower() == "b":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was B, Japan")
+ready = input("Press enter to move onto the next question ")
+
+# Question 8
+print("")
+print("")
+print("")
+print("Question 8")
+print('Who painted the famous artwork "Mona Lisa"?')
+print("")
+print("A: Vincent van Gogh")
+print("")
+print("B: Pablo Picasso")
+print("")
+print("C: Leonardo da Vinci")
+print("")
+print("D: Michelangelo")
+print("")
+
+Q8_Answer = input(">>> ")
+
+if Q8_Answer.lower() == "c":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was C, Leonardo da Vinci")
+ready = input("Press enter to move onto the next question ")
+
+# Question 9
+print("")
+print("")
+print("")
+print("Question 9")
+print('What is the currency of Japan?')
+print("")
+print("A: Yen")
+print("")
+print("B: Euro")
+print("")
+print("C: Dollar")
+print("")
+print("D: Pound")
+print("")
+
+Q9_Answer = input(">>> ")
+
+if Q9_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, Yen")
+ready = input("Press enter to move onto the next question ")
+
+# Question 10
+print("")
+print("")
+print("")
+print("Question 10")
+print('Which gas do plants absorb during photosynthesis?')
+print("")
+print("A: Oxygen")
+print("")
+print("B: Nitrogen")
+print("")
+print("C: Carbon dioxide")
+print("")
+print("D: Hydrogen")
+print("")
+
+Q10_Answer = input(">>> ")
+
+if Q10_Answer.lower() == "c":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was C, Carbon dioxide")
+ready = input("Press enter to move onto the next question ")
+
+# Question 11
+print("")
+print("")
+print("")
+print("Question 11")
+print('Who is the author of “Pride and Prejudice”?')
+print("")
+print("A: Emily Brontë")
+print("")
+print("B: Jane Austen")
+print("")
+print("C: Charlotte Brontë")
+print("")
+print("D: Louisa May Alcott")
+print("")
+
+Q11_Answer = input(">>> ")
+
+if Q11_Answer.lower() == "b":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was B, Jane Austen")
+ready = input("Press enter to move onto the next question ")
+
+# Question 12
+print("")
+print("")
+print("")
+print("Question 12")
+print('What is the largest ocean on Earth?')
+print("")
+print("A: Atlantic Ocean")
+print("")
+print("B: Indian Ocean")
+print("")
+print("C: Pacific Ocean")
+print("")
+print("D: Arctic Ocean")
+print("")
+
+Q12_Answer = input(">>> ")
+
+if Q12_Answer.lower() == "c":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was C, Pacific Ocean")
+ready = input("Press enter to move onto the next question ")
+
+# Question 13
+print("")
+print("")
+print("")
+print("Question 13")
+print('Which famous scientist discovered the law of gravity?')
+print("")
+print("A: Isaac Newton")
+print("")
+print("B: Albert Einstein")
+print("")
+print("C: Galileo Galilei")
+print("")
+print("D: Nikola Tesla")
+print("")
+
+Q13_Answer = input(">>> ")
+
+if Q13_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, Isaac Newton")
+ready = input("Press enter to move onto the next question ")
+
+# Question 14
+print("")
+print("")
+print("")
+print("Question 14")
+print('What is the chemical symbol for gold?')
+print("")
+print("A: Au")
+print("")
+print("B: Ag")
+print("")
+print("C: Fe")
+print("")
+print("D: Cu")
+print("")
+
+Q14_Answer = input(">>> ")
+
+if Q14_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, Au")
+ready = input("Press enter to move onto the next question ")
+
+# Question 15
+print("")
+print("")
+print("")
+print("Question 15")
+print('What year did the first man step on the moon?')
+print("")
+print("A: 2021")
+print("")
+print("B: 1979")
+print("")
+print("C: 1996")
+print("")
+print("D: 1969")
+print("")
+
+Q15_Answer = input(">>> ")
+
+if Q15_Answer.lower() == "d":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was D, 1969")
+ready = input("Press enter to move onto the next question ")
+
+# Question 16
+print("")
+print("")
+print("")
+print("Question 16")
+print('Which animal is known as the “King of the Jungle”?')
+print("")
+print("A: Elephant")
+print("")
+print("B: Lion")
+print("")
+print("C: Tiger")
+print("")
+print("D: Gorilla")
+print("")
+
+Q16_Answer = input(">>> ")
+
+if Q16_Answer.lower() == "b":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was B, Lion")
+ready = input("Press enter to move onto the next question ")
+
+# Question 17
+print("")
+print("")
+print("")
+print("Question 17")
+print('What is the largest planet in our solar system?')
+print("")
+print("A: Earth")
+print("")
+print("B: Mars")
+print("")
+print("C: Jupiter")
+print("")
+print("D: Saturn")
+print("")
+
+Q17_Answer = input(">>> ")
+
+if Q17_Answer.lower() == "c":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was C, Jupiter")
+ready = input("Press enter to move onto the next question ")
+
+# Question 18
+print("")
+print("")
+print("")
+print("Question 18")
+print('Who wrote the famous novel “1984”?')
+print("")
+print("A: George Orwell")
+print("")
+print("B: Aldous Huxley")
+print("")
+print("C: Ray Bradbury")
+print("")
+print("D: J.D. Salinger")
+print("")
+
+Q18_Answer = input(">>> ")
+
+if Q18_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, George Orwell")
+ready = input("Press enter to move onto the next question ")
+
+# Question 19
+print("")
+print("")
+print("")
+print("Question 19")
+print('What is the chemical symbol for silver?')
+print("")
+print("A: Ag")
+print("")
+print("B: Au")
+print("")
+print("C: Pt")
+print("")
+print("D: S")
+print("")
+
+Q19_Answer = input(">>> ")
+
+if Q19_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, Ag")
+ready = input("Press enter to move onto the next question ")
+
+# Question 20
+print("")
+print("")
+print("")
+print("Last Question: Question 20")
+print('Which famous scientist developed the theory of evolution by natural selection?')
+print("")
+print("A: Charles Darwin")
+print("")
+print("B: Gregor Mendel")
+print("")
+print("C: Alfred Russel Wallace")
+print("")
+print("D: Thomas Edison")
+print("")
+
+Q20_Answer = input(">>> ")
+
+if Q20_Answer.lower() == "a":
+    print("Correct")
+    score += 1
+else:
+    print("Wrong")
+    print("The correct answer was A, Charles Darwin")
+ready = input("Press enter to finish the quiz ")
+
+#End / Score
+
+print("")
+print("")
+print("")
+print("You have reached the end of the quiz")
+
+if score == 0:
+    print("Your score was: " + str(score) + "/20")
+    print("Do you even have a brain?")
+
+elif score <= 5:
+    print("Your score was: " + str(score) + "/20")
+    print("At least you tried.")
+    print("But you still failed miserably.")
+
+elif score <= 10:
+    print("Your score was: " + str(score) + "/20")
+    print("Maybe stick to less brain-involving hobbies.")
+    print("Doing quizzes certainly isn't one of your strengths.")
+
+elif score <= 15:
+    print("Your score was: " + str(score) + "/20")
+    print("At least you got over 50%")
+    
+elif score <= 19:
+    print("Your score was: " + str(score) + "/20")
+    print("Well done!")
+
+elif score == 20:
+    print("Your score was: " + str(score) + "/20")
+    print("You got 100%")
+    print("You are a genius!")
+
+
+
+
+
+
+
+    
